b2aea2c51341ed4f596984ec65535da7daf4019d
Ramiro Andres Garcia Urueta <ramirogarciaurueta1005@gmail.com>
2024-03-11 20:05:37 -0500

    feat: :sparkles: Implementacion de  un dataset de articulos
    Se implemento de forma manual un dataset de articulos

---
+++ js/data/articleSportsData.js
@@ -0,0 +1,39 @@
+export const sportsArticles = [
+    {
+      name: "Basketball Ball",
+      price: 24.99,
+      description: "Regulation-size basketball made of durable rubber, suitable for indoor and outdoor use. Designed for optimal bounce and grip, perfect for recreational play and competitive games.",
+      img_url: "https://media.istockphoto.com/id/610241662/es/foto/bal%C3%B3n-de-f%C3%BAtbol-aislado-sobre-el-fondo-blanco.jpg?s=612x612&w=0&k=20&c=m694Qtfj2li0GhXfMijPEKdKtnw52_kXZxPMbH9ZOXM="
+    },
+    {
+      name: "Soccer Ball",
+      price: 19.99,
+      description: "Classic soccer ball featuring a durable synthetic leather cover and reinforced rubber bladder for enhanced shape retention and durability. Suitable for all skill levels and playing surfaces.",
+      img_url: "https://i5.walmartimages.com/asr/69d0b61d-8813-4565-854c-64731b7f9f29.c085d11aaa6e9958a673bf4ac5ef8fef.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
+    },
+    {
+      name: "Tennis Racket",
+      price: 79.99,
+      description: "High-performance tennis racket crafted from lightweight graphite material, offering excellent control and power. Features a comfortable grip and large sweet spot for improved accuracy.",
+      img_url: "https://target.scene7.com/is/image/Target/GUEST_a9307de1-8b92-4b68-a2c3-1affc025b94d?wid=488&hei=488&fmt=pjpeg"
+    },
+    {
+      name: "Golf Clubs Set",
+      price: 299.99,
+      description: "Complete set of golf clubs including drivers, irons, wedges, and putters. Constructed from premium materials for maximum distance, accuracy, and forgiveness. Ideal for golfers of all skill levels.",
+      img_url: "https://cdn11.bigcommerce.com/s-p2fi4ihyaw/images/stencil/1280x1280/products/365/2255/6f82aaa8-2d77-4dad-92b4-88d38c3506f1__52623.1663735838.jpg?c=1"
+    },
+    {
+      name: "Running Shoes",
+      price: 89.99,
+      description: "Lightweight and breathable running shoes with cushioned midsoles for superior comfort and shock absorption. Durable rubber outsoles provide excellent traction on various surfaces. Ideal for long-distance running and daily workouts.",
+      img_url: "https://cdn.thewirecutter.com/wp-content/media/2023/09/running-shoes-2048px-5946.jpg?auto=webp&quality=75&width=1024"
+    },
+    {
+      name: "Baseball Glove",
+      price: 34.99,
+      description: "Classic baseball glove made from genuine leather for durability and performance. Features padded palm and finger linings for added comfort and protection. Suitable for fielding and catching.",
+      img_url: "https://ca.b45baseball.com/cdn/shop/products/ProSeries-I-Web-Tan_1_1991x.jpg?v=1617829729"
+    }
+  ]
+
