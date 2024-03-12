c3cc80246d7b9c6537ae6707b16db4793ecf45fd
Ramiro Andres Garcia Urueta <ramirogarciaurueta1005@gmail.com>
2024-03-11 20:28:59 -0500

    feat: :sparkles: se agregaron mas datos a los articulos

--- js/data/articleSportsData.js
+++ js/data/articleSportsData.js
@@ -34,6 +34,24 @@ export const sportsArticles = [
       price: 34.99,
       description: "Classic baseball glove made from genuine leather for durability and performance. Features padded palm and finger linings for added comfort and protection. Suitable for fielding and catching.",
       img_url: "https://ca.b45baseball.com/cdn/shop/products/ProSeries-I-Web-Tan_1_1991x.jpg?v=1617829729"
+    },
+    {
+      name: "Golf Clubs Set",
+      price: 299.99,
+      description: "Complete set of golf clubs including drivers, irons, wedges, and putters. Constructed from premium materials for maximum distance, accuracy, and forgiveness. Ideal for golfers of all skill levels.",
+      img_url: "https://cdn11.bigcommerce.com/s-p2fi4ihyaw/images/stencil/1280x1280/products/365/2255/6f82aaa8-2d77-4dad-92b4-88d38c3506f1__52623.1663735838.jpg?c=1"
+    },
+    {
+      name: "Running Shoes",
+      price: 89.99,
+      description: "Lightweight and breathable running shoes with cushioned midsoles for superior comfort and shock absorption. Durable rubber outsoles provide excellent traction on various surfaces. Ideal for long-distance running and daily workouts.",
+      img_url: "https://cdn.thewirecutter.com/wp-content/media/2023/09/running-shoes-2048px-5946.jpg?auto=webp&quality=75&width=1024"
+    },
+    {
+      name: "Baseball Glove",
+      price: 34.99,
+      description: "Classic baseball glove made from genuine leather for durability and performance. Features padded palm and finger linings for added comfort and protection. Suitable for fielding and catching.",
+      img_url: "https://ca.b45baseball.com/cdn/shop/products/ProSeries-I-Web-Tan_1_1991x.jpg?v=1617829729"
     }
   ]
 
