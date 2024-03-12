e8fbc11da441a34674a84c540b7a8ec7750836b2
Ramiro Andres Garcia Urueta <ramirogarciaurueta1005@gmail.com>
2024-03-11 20:31:11 -0500

    feat:

---
+++ js/index.js
@@ -0,0 +1,27 @@
+import { sportsArticles } from "./data/articleSportsData.js";
+
+(() => {
+    const cardsContainer = document.getElementById("card-container");
+
+    const createCardItem = ({
+        name,
+        price,
+        description,
+        img_url
+    }) => {
+        return `<div class="card" style="width: 18rem;">
+        <img src="${img_url}" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <p class="card-text">${description}</p>
+          <a href="#" class="btn btn-primary">Buy $ ${price}</a>
+        </div>
+      </div>`
+    }
+
+    sportsArticles.forEach((article) => {
+        const item = createCardItem(article);
+
+        cardsContainer.innerHTML += item;
+    })
+})()
